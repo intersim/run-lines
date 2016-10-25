7dfbca77624bb4d7322e41104a447bf3d007b200
intersim <emily.intersimone@gmail.com>
2016-10-24 21:43:33 -0400

    split JSON files

--- server/app/index.js
+++ server/app/index.js
@@ -1,14 +1,19 @@
 'use strict'; 
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const TOC = require('../../data/toc.json');
 
-var indexPath = path.join(__dirname, '..', '..', 'browser', 'index.html');
+const indexPath = path.join(__dirname, '..', '..', 'browser', 'index.html');
 
 app.use(express.static('public'));
 
-app.use(function (req, res) {
+app.all('/', function (req, res) {
   res.sendFile(indexPath);
+});
+
+toc.forEach(title => {
+	app.get(`/$title`, (req, res, next) => res.json(require(`../../data/${title}.json`)));
 });
 
 module.exports = app;
--- splitJSON.js
+++ splitJSON.js
@@ -1,25 +1,31 @@
 // write line obj to file with same name as "play_name"
 
 const fs = require('fs');
+const _ = require('lodash');
+
 
 const splitJSON = (a) => {
-	a.forEach(lineObj => {
-		console.log(`Writing ${lineObj.play_name}...`)
-		const err = fs.writeFileSync(`./data/${lineObj.play_name}.json`, JSON.stringify(lineObj))
+	const playsObj = _.groupBy(a, 'play_name');
+
+	for (title in playsObj) {
+		console.log(`Writing ${title}...`)
+		const filename = title.replace(/\s/g, "-");
+		const err = fs.writeFileSync(`./data/${filename}.json`, JSON.stringify(playsObj[title]))
 		if (err) console.error(err);
-		console.log(`Wrote ${lineObj.play_name}.`)
-	});
+		console.log(`Wrote ${title}.`)
+	}
+
 	console.log("all done splitting JSON!");
 }
 
 console.log("Getting ready to split up this JSON...");
 
-splitJSON(
-  JSON.parse(
-  fs.readFileSync('./data/will_play_text.json')
-     .toString()
-     .replace(/\\'/g, '')
-     .replace(/\t/g, ' ')))
+// splitJSON(
+//   JSON.parse(
+//   fs.readFileSync('./data/will_play_text.json')
+//      .toString()
+//      .replace(/\\'/g, '')
+//      .replace(/\t/g, ' ')))
  
-//const playArray = require('./data/will_play_text.fixed.json');
-//splitJSON(playArray);
+const playArray = require('./data/will_play_text.fixed.json');
+splitJSON(playArray);
