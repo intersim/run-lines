ac8cb1bc4b4928eb05317aac691063c287c7b5fb
Emily Intersimone <emily.intersimone@gmail.com>
2017-02-02 15:53:28 -0500

    slight refactor of routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,21 +1,17 @@
 const router = require('express').Router();
 
-const toc = require('../../data/plays/toc.json');
+router.get(`/plays/:play`, (req, res, next) => {
+	res.json(require(`../../data/plays/${req.params.play}.json`));
+});
 
-toc.forEach(play => {
-	router.get(`/plays/${play}`, (req, res, next) => {
-		res.json(require(`../../data/plays/${play}.json`));
-	});
+router.get(`/plays/:play/characters`, (req, res, next) => {
+	res.json(require(`../../data/characters/${req.params.play}-characters.json`));
+});
 
-	router.get(`/plays/${play}/acts/:actNum/scenes/:sceneNum`, (req, res, next) => {
-		const actNum = req.params.actNum;
-		const sceneNum = req.params.sceneNum;
-		res.json(require(`../../data/plays/${play}/act_${actNum}/scene_${sceneNum}.json`));
-	});
-
-	router.get(`/plays/${play}/characters`, (req, res, next) => {
-		res.json(require(`../../data/characters/${play}-characters.json`));
-	});
+router.get(`/plays/:play/acts/:actNum/scenes/:sceneNum`, (req, res, next) => {
+	const actNum = req.params.actNum;
+	const sceneNum = req.params.sceneNum;
+	res.json(require(`../../data/plays/${req.params.play}/act_${actNum}/scene_${sceneNum}.json`));
 });
 
 module.exports = router;
