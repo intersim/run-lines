211eb978e072353106cf89644840caf3ae478fa6
Emily Intersimone <emily.intersimone@gmail.com>
2017-02-02 12:31:08 -0500

    splitPlayIntoActsAndScenes appears to be working

--- utils/splitPlayIntoActs.js
+++ utils/splitPlayIntoActs.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
 const toc = require('../data/plays/toc.json');
+const TwelfthNight = require('../data/plays/Twelfth-Night/Twelfth-Night.json');
 const path = require('path');
 
 // const makeFolders = () => {
 // 	toc.forEach(play => fs.mkdir(
-// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts'), 
+// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts'),
 // 		err => {
 // 			if (err) console.log(`Something went wrong: ${err}\n`)
 // 		})
 // 	)
-// 
+//
 // for (var i = 1; i <= 5; i++) {
 // 	toc.forEach(play => fs.mkdir(
-// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts', i), 
+// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts', i),
 // 		err => {
 // 			if (err) console.log(`Something went wrong: ${err}\n`)
 // 		})
 // 	)
 // }
-// 
+//
 // 	toc.forEach(play => fs.mkdir(
-// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts', 'scenes'), 
+// 		path.join(__dirname, '..', 'data', 'plays', play, 'acts', 'scenes'),
 // 		err => {
 // 			if (err) console.log(`Something went wrong: ${err}\n`)
 // 		})
@@ -30,29 +31,67 @@ const path = require('path');
 const splitPlayIntoActsAndScenes = play => {
 	const splitPlay = {}
 
-	let lastAct = 1
-	let lastScene = 1
+	let lastAct = 0
+	let lastScene = 0
 	let emptyLineNum = 0
 
 	play.forEach(line => {
+		// increment last
+		if (line.text_entry.toLowerCase().includes("act")) {
+			lastAct++;
+		}
 
-	// for stage directions that don't have a line number:
-		if (!line.line_number) line.line_number = [lastAct, lastScene, emptyLineNum].join('.')
-		if (line.text_entry.includes("ACT")) lastAct++, lastScene++
+		if (line.text_entry.toLowerCase().includes("scene")) {
+			lastScene++;
+		}
 
-		const [act, scene] = line.line_number.split(".").map(str => Number(str))
-		if (!splitPlay[`act_${act}`]) splitPlay[`act_${act}`] = {}
-		if (!splitPlay[`act_${act}`][`scene_${scene}`]) splitPlay[`act_${act}`][`scene_${scene}`] = []
-		splitPlay[`act_${act}`][`scene_${scene}`].push(line)
+		// for stage directions that don't have a line number:
+		let act, scene;
+		if (!line.line_number) {
+			line.line_number = [lastAct, lastScene, emptyLineNum].join('.')
+			act = lastAct;
+			scene = lastScene;
+		} else {
+			[act, scene] = line.line_number.split(".").map(str => Number(str));
+		}
+
+		if (!splitPlay[`act_${act}`]) {
+			splitPlay[`act_${act}`] = {}
+		}
+
+		if (!splitPlay[`act_${act}`][`scene_${scene}`]) {
+			splitPlay[`act_${act}`][`scene_${scene}`] = [];
+		}
+
+		splitPlay[`act_${act}`][`scene_${scene}`].push(line);
+
+		lastAct = act;
+		lastScene = scene;
 	})
+
+	return splitPlay;
 }
 
-const writeSceneToFile = (sceneArr, sceneNum) => {
+console.log(JSON.stringify(splitPlayIntoActsAndScenes(TwelfthNight)[`act_1`][`scene_2`]));
 
+const writePlayScenesToFile = (playObj) => {
+	const playName = playObj.act_1.scene_1[0].play_name.replace(/\s/g, "-");
+
+	for (let act in play) {
+		// make a folder for the current act
+		const actPath = path.join(__dirname, '..', 'data', 'plays', playName, act);
+
+		fs.mkdir(actPath, err => {
+			if (err) console.log(`Something went wrong: ${err}\n`)
+		})
+
+		// write scenes to file in that folder
+		for (let scene in act) {
+			const err = fs.writeFileSync(`../data/plays/${playName}/${act}/${scene}.json`)
+			if (err) console.error(err);
+			console.log(`Wrote ${scene}.`)
+		}
+		console.log(`Finished with ${act}`!)
+	}
+	console.log(`All done with ${playName}`)
 }
-
-const writeActToFile = (actObj, actNum) => {
-
-}
-
-const splitAllPlays
