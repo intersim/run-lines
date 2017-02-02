34fea5e389a344bcb4a92f4a43e891b1b61b4848
Emily Intersimone <emily.intersimone@gmail.com>
2017-02-02 15:34:47 -0500

    add more routes

--- server/api/index.js
+++ server/api/index.js
@@ -5,13 +5,12 @@ const toc = require('../../data/plays/toc.json');
 toc.forEach(play => {
 	router.get(`/plays/${play}`, (req, res, next) => {
 		res.json(require(`../../data/plays/${play}.json`));
-		// ^^ is this bad practice...?
 	});
 
-	// router.get(`/plays/${play}/acts/:actNum`, (req, res, next) => {
-	// 	const actNum = req.params.actNum;
-	// 	res.json(require(`../../data/characters/${play}-act-${actNum}.json`));
-	// });
+	router.get(`/plays/${play}/acts/:actNum/scenes/:sceneNum`, (req, res, next) => {
+		const actNum = req.params.actNum;
+		res.json(require(`../../data/plays/${play}/act_${actNum}/scene_${sceneNum}.json`));
+	});
 
 	router.get(`/plays/${play}/characters`, (req, res, next) => {
 		res.json(require(`../../data/characters/${play}-characters.json`));
