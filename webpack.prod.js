84504484f730808c69c50bbd51c0163175d24f9d
intersim <emily.intersimone@gmail.com>
2017-09-08 19:54:57 -0400

    update webpack config, npm scripts

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(common, {
+  plugins: [
+	  new webpack.DefinePlugin({
+		  'process.env': {
+		    NODE_ENV: JSON.stringify('production')
+		  }
+		}),
+    new UglifyJSPlugin()
+  ]
+});
