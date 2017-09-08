8102d996e84b0f2e579d2a7b266d6a1d35b649fb
intersim <emily.intersimone@gmail.com>
2017-09-08 19:18:57 -0400

    add compression middleware, remove redux logging middleware

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const compression = require('compression');
 
 const indexPath = path.join(__dirname, '..', '..', 'browser', 'index.html');
 
@@ -10,6 +11,8 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(compression());
 
 app.use(express.static('public', {
   maxAge: 86400000 // one day in milliseconds
